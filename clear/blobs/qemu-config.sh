6427a8cb4399029946b3a4cd4ebfb9bf69e04947
Dan Kaminsky <dan@doxpara.com>
2016-10-29 09:11:50 +0000

    Apparently you can't debug.  Great.

--- clear/blobs/qemu-config.sh
+++ clear/blobs/qemu-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=/usr --enable-debug --disable-libssh2 --disable-tcmalloc --disable-glusterfs \
+./configure --prefix=/usr --disable-libssh2 --disable-tcmalloc --disable-glusterfs \
     --enable-seccomp --disable-{bzip2,snappy,lzo} --disable-usb-redir \
     --disable-libusb --disable-libnfs --disable-tcg-interpreter --disable-debug-tcg \
     --disable-libiscsi --disable-rbd --disable-spice --enable-attr \
