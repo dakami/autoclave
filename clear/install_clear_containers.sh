8404a034ff5f0b7c83525e09021b94224bb44931
Dan Kaminsky <dan@doxpara.com>
2016-10-29 08:32:52 +0000

    Cleanup

---
+++ clear/install_clear_containers.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install libpixman-1-0
+
+sh -c "echo 'deb http://download.opensuse.org/repositories/home:/clearlinux:/preview:/clear-containers-2.0/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/cc-oci-runtime.list"
+
+
+wget http://download.opensuse.org/repositories/home:clearlinux:preview:clear-containers-2.0/xUbuntu_16.04/Release.key
+apt-key add Release.key
+rm Release.key
+
+apt-get update
+apt-get -y install cc-oci-runtime
+
+apt-get update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get purge lxc-docker
+apt-cache policy docker-engine
+apt-get install docker-engine=1.12.1-0~xenial
+
+pushd /usr/share/clear-containers/
+rm clear-containers.img
+ln -s clear-*-containers.img clear-containers.img
+sed -ie 's!"image":.*$!"image": "/usr/share/clear-containers/clear-containers.img",!g' /usr/share/defaults/cc-oci-runtime/vm.json
+popd
+
+mkdir -p /etc/systemd/system/docker.service.d/
+cp blobs/clr-containers.conf /etc/systemd/system/docker.service.d/
+
+systemctl daemon-reload
+systemctl restart docker
+
+
+
+
+
+
---
+++ clear/install_qemu_lite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cat /etc/apt/sources.list | sed 's/deb /deb-src /g' > /etc/apt/sources.list.d/sources.list
+apt-get update
+
+apt-get -y build-dep qemu
+
+git clone https://github.com/01org/qemu-lite.git
+cd qemu-lite
+git checkout qemu-2.7-lite
+git reset --hard qemu-2.7-lite
+cp -p ../blobs/qemu-config.sh .
+patch -p1 < ../../patches_to_be_migrates/vl_autoclave.patch
+./qemu-config.sh
+make
+make install
+rm /usr/bin/qemu-lite-system-x86_64 /usr/bin/qemu-lite-ga /usr/bin/virtfs-lite-proxy-helper
+ln -s /usr/bin/qemu-system-x86_64 /usr/bin/qemu-lite-system-x86_64
+ln -s /usr/bin/qemu-ga /usr/bin/qemu-lite-ga
+ln -s /usr/bin/virtfs-proxy-helper /usr/bin/virtfs-lite-proxy-helper
+
