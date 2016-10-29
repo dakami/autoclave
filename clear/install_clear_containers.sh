a0b23a6797a520fd0e0335377231e8468fcbaa75
Dan Kaminsky <dan@doxpara.com>
2016-10-29 09:36:14 +0000

    scrubbing

--- clear/install_clear_containers.sh
+++ clear/install_clear_containers.sh
@@ -14,7 +14,7 @@ apt-get update
 apt-get -y install cc-oci-runtime
 
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get -y install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list"
 apt-get update
